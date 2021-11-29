6ab1b5562450562027c450d655fe1f513a3ba3ed
Jesse Meng <mengfj@gmail.com>
2021-11-29 22:59:58 +0800

    init commit

---
+++ service1/main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"io"
+	"math/rand"
+	"net/http"
+	_ "net/http/pprof"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/golang/glog"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+
+	"github.com/cncamp/golang/httpserver/metrics"
+)
+
+func main() {
+	flag.Set("v", "4")
+	glog.V(2).Info("Starting http server...")
+	metrics.Register()
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/hello", rootHandler)
+	mux.HandleFunc("/healthz", healthz)
+	mux.Handle("/metrics", promhttp.Handler())
+
+	srv := http.Server{
+		Addr:    ":80",
+		Handler: mux,
+	}
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			glog.Fatalf("listen: %s\n", err)
+		}
+	}()
+	glog.Info("Server Started")
+	<-done
+	glog.Info("Server Stopped")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer func() {
+		// extra handling here
+		cancel()
+	}()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		glog.Fatalf("Server Shutdown Failed:%+v", err)
+	}
+	glog.Info("Server Exited Properly")
+}
+
+func healthz(w http.ResponseWriter, r *http.Request) {
+	for k, v := range r.Header {
+		io.WriteString(w, fmt.Sprintf("%s=%s\n", k, v))
+	}
+
+	io.WriteString(w, "ok\n")
+}
+
+func randInt(min int, max int) int {
+	rand.Seed(time.Now().UTC().UnixNano())
+	return min + rand.Intn(max-min)
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	glog.V(4).Info("entering v2 root handler")
+	timer := metrics.NewTimer()
+	defer timer.ObserveTotal()
+	user := r.URL.Query().Get("user")
+	if user == "" {
+		user = r.URL.Query().Get("User")
+	}
+	delay := randInt(10,2000)
+	time.Sleep(time.Millisecond*time.Duration(delay))
+	if user != "" {
+		io.WriteString(w, fmt.Sprintf("hello [%s]\n", user))
+	} else {
+		io.WriteString(w, "hello [stranger]\n")
+	}
+	io.WriteString(w, "===================Details of the http request header:============\n")
+	for k, v := range r.Header {
+		io.WriteString(w, fmt.Sprintf("%s=%s\n", k, v))
+	}
+	glog.V(4).Infof("Respond in %d ms", delay)
+}
